23df01b2d018cb89ca138d268b75fda0316358d7
Seb Gertz <sebgertz@gmail.com>
2018-09-04 23:34:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function findMinAndRemove(firstHalf, secondHalf){
   let minfirstHalf = firstHalf[0]
   let minsecondHalf = secondHalf[0]
- 
+
   if(minfirstHalf < minsecondHalf){
     return firstHalf.shift()
   } else {
