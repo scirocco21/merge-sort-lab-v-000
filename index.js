a1e4b0316a6f01ef32a45d941f1068893322ee9b
Seb Gertz <sebgertz@gmail.com>
2018-09-04 23:35:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function mergeSort(array){
     let midpoint = array.length/2
     let firstHalf = array.slice(0, midpoint)
     let secondHalf = array.slice(midpoint, array.length)
- 
+
     if(array.length < 2){
       return array
     } else {
