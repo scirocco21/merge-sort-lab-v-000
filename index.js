9e6403ff03584048868fc8d8e6387be74dced7b5
Seb Gertz <sebgertz@gmail.com>
2018-09-04 23:31:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function findMinAndRemove(firstHalf, secondHalf){
+  let minfirstHalf = firstHalf[0]
+  let minsecondHalf = secondHalf[0]
+ 
+  if(minfirstHalf < minsecondHalf){
+    return firstHalf.shift()
+  } else {
+    return secondHalf.shift()
+  }
+}
 
+
+function merge(firstHalf, secondHalf){
+  let sorted = []
+  let currentMin;
+  while(firstHalf.length != 0 && secondHalf.length != 0){
+    let currentMin = findMinAndRemove(firstHalf, secondHalf)
+    sorted.push(currentMin)
+  }
+  return sorted.concat(firstHalf).concat(secondHalf)
+}
